bf96c945f2162664b712acfaad36bd271d1c9bae
liuxiao <liuxiao2103@qq.com>
2021-10-30 00:58:04 +0800

    feat: 局部变量的线程安全问题

---
+++ src/com/liuxiaocs/jvm/t1/Demo1_17.java
@@ -0,0 +1,49 @@
+package com.liuxiaocs.jvm.t1;
+
+/**
+ * 局部变量的线程安全问题
+ */
+public class Demo1_17 {
+    public static void main(String[] args) {
+        // 主线程修改StringBuilder对象
+        StringBuilder sb = new StringBuilder();
+        sb.append(4);
+        sb.append(5);
+        sb.append(6);
+
+        // 子线程同时修改StringBuilder对象
+        new Thread(() -> {
+            m2(sb);
+        }).start();
+    }
+
+    // 不会有线程安全问题
+    // sb是线程内的局部变量，是属于每个线程私有的
+    // 其他线程不可能同时访问到这个StringBuilder对象，所以它一定是线程安全的
+    public static void m1() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(1);
+        sb.append(2);
+        sb.append(3);
+        System.out.println(sb.toString());
+    }
+
+    // 有可能多个线程都能访问到sb这个对象
+    // 多个线程共享同一个对象，因此不是线程安全的
+    // 不能使用StringBuilder了，需要改写成StringBuffer的实现
+    public static void m2(StringBuilder sb) {
+        sb.append(1);
+        sb.append(2);
+        sb.append(3);
+        System.out.println(sb.toString());
+    }
+
+    // 其他对象有可能拿到返回的这个对象的引用并发的去修改它造成线程安全的问题
+    public static StringBuilder m3() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(1);
+        sb.append(2);
+        sb.append(3);
+        return sb;
+    }
+}
---
+++ src/com/liuxiaocs/jvm/t1/Demo1_18.java
@@ -0,0 +1,17 @@
+package com.liuxiaocs.jvm.t1;
+
+/**
+ * 局部变量的线程安全问题
+ */
+public class Demo1_18 {
+
+    // 多个线程同时执行此方法
+    static void m1() {
+        // 方法内局部变量x
+        int x = 0;
+        for(int i = 0; i < 5000; i++) {
+            x++;
+        }
+        System.out.println(x);
+    }
+}
