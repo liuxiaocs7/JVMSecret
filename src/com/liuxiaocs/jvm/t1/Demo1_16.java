b98550b7a2dcffd81e190faeab3b7f4a3f05c6da
liuxiao <liuxiao2103@qq.com>
2021-11-04 23:47:35 +0800

    feat: 演示CPU占用过高

---
+++ src/com/liuxiaocs/jvm/t1/Demo1_16.java
@@ -0,0 +1,34 @@
+package com.liuxiaocs.jvm.t1;
+
+/**
+ * 演示CPU占用过高
+ */
+public class Demo1_16 {
+
+    public static void main(String[] args) {
+        new Thread(null, () -> {
+            System.out.println("1...");
+            while(true) {
+
+            }
+        }, "thread1").start();
+
+        new Thread(null, () -> {
+            System.out.println("2...");
+            try {
+                Thread.sleep(1000000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "thread2").start();
+
+        new Thread(null, () -> {
+            System.out.println("3...");
+            try {
+                Thread.sleep(1000000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "thread3").start();
+    }
+}
